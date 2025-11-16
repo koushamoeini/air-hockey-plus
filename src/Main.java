62ab32ea3b5f9339424eeee8e3d69fa07d6dec6c
koush <koushamoeini@gmail.com>
2025-11-16 15:18:51 +0330

    refactor: reorganize project structure

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-    static History history=new History();
-    public static void main(String[]args){
-       new MainMenu();
-       history.history.setVisible(false);
-    }
-}
-
